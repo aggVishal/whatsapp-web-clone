fc462e7ec37299587d7209064d7f6f50022778c4
Vishal Aggarwal <aggarwalvishal910@outlook.com>
2022-10-20 18:35:09 +0530

    Basic features done

--- backend/main.js
+++ backend/main.js
@@ -14,11 +14,17 @@ http.listen(8000, function() {
         socket.on('new-user-joined', name => {
             users[socket.id] = name;
             socket.broadcast.emit('user-joined', name);
-            console.log("new user joined", name);
+            console.log(users);
         });
 
         socket.on('send', message => {
             socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
         });
-    })
+
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('user-left', users[socket.id]);
+            delete users[socket.id];
+        });
+    });
+
 })
--- frontend/client.js
+++ frontend/client.js
@@ -4,22 +4,33 @@ const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
 
-var sendAudio = new Audio('assets\audios\messageOutgoing.mp3')
 
-const append = (message, position) => {
+const appendMessage = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if (position == 'right') {
-        sendAudio.play();
-    }
 }
 
 const name = prompt("Enter your name");
 socket.emit('new-user-joined', name);
 
 socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'right');
+    appendMessage(`${name} joined the chat`, 'right');
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
+
+socket.on('receive', data => {
+    appendMessage(`${data.name}: ${data.message}`, 'left');
+})
+socket.on('user-left', name => {
+    appendMessage(`${name} left the chat`, 'left');
 })
